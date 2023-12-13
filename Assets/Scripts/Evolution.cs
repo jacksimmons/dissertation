51ef8041588bfbacae23e3dbf15291078fcd4ae1
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2023-12-13 16:04:28 +0000

    unity migration

---
+++ Assets/Scripts/Evolution.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+
+namespace EA;
+
+// 32-Bit Bit Field.
+
+// Saves on memory usage for boolean values
+// bool = 1 byte, but T/F can be stored in 1 bit.
+// + BitField can store data 8x more memory efficiently.
+// - Accessing a single value is more computationally expensive.
+// + But performing bitwise operations negates this issue.
+//public class BitField8
+//{
+//    // 8 bit integer - 8 fields
+//    // Undefined behaviour if !(0 <= index <= 7)
+//    public byte Data { get; private set; }
+
+//    // Set every bit to 0 by default
+//    public BitField8(byte data = 0) { Data = data; }
+
+//    public void SetBit(int index, bool value)
+//    {
+//        byte mask = (byte)(1 << index);
+//        Data = (byte)(value ? (Data | mask) : (Data & ~mask));
+//    }
+
+//    public bool GetBit(int index)
+//    {
+//        int mask = 1 << index;
+//        return (Data & mask) != 0;
+//    }
+//}
+
+
+public class Evolution
+{
+    private bool Vegetarian;
+    private bool Pescatarian;
+    private bool Vegan;
+    private bool NoDairy;
+
+
+    public Evolution(
+        bool vegetarian = false,
+        bool pescatarian = false,
+        bool vegan = false,
+        bool noDairy = false)
+    {
+        Vegetarian = vegetarian;
+        Pescatarian = pescatarian;
+        Vegan = vegan;
+        NoDairy = noDairy;
+    }
+
+
+    private bool IsFoodGroupAllowed(string foodGroup)
+    {
+        // Categories
+        switch (foodGroup[0])
+        {
+            case 'M': // Meat
+                if (Vegetarian || Pescatarian || Vegan)
+                    return false;
+                break;
+            case 'J': // Fish
+                if (Vegetarian || Vegan)
+                    return false;
+                break;
+            case 'C': // Eggs
+                if (Vegan)
+                    return false;
+                break;
+            case 'B': // Milk
+                if (Vegan || NoDairy)
+                    return false;
+                break;
+            case 'Q': // Alcohol - Excluded
+                return false;
+        }
+
+        // Unique cases
+        switch (foodGroup)
+        {
+            case "OB": // Animal fats
+                if (Vegetarian || Pescatarian || Vegan)
+                    return false;
+                break;
+        }
+
+        return true;
+    }
+
+
+    public List<Portion> ExtractProximates()
+    {
+        int numMissingDataFields = 0;
+
+        float GetProximate(string field, ref bool error)
+        {
+            if (field == "Tr" || field == "")
+                return 0f;
+            
+            // Insufficient data; disregard
+            if (field == "N")
+            {
+                numMissingDataFields++;
+                error = true;
+                return 0f;
+            }
+
+            return float.Parse(field);
+        }
+
+        //https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp
+        using (TextFieldParser parser = new(@"C:\Git\dissertation\Dis\Evolution\Proximates.csv"))
+        {
+            List<Portion> portions = new();
+
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            int rowNum = -1;
+
+            while (!parser.EndOfData)
+            {
+                // Process row
+                string[]? fields = parser.ReadFields();
+                rowNum++;
+                
+                if (fields == null || rowNum < 3)
+                {
+                    continue;
+                }
+
+                bool missingData = false;
+                bool dietaryConflict = false;
+
+                string name = "Unnamed";
+                float protein = 0;
+                float fat = 0;
+                float carbs = 0;
+                float energy = 0;
+                float saturates = 0;
+                float sugars = 0;
+                float trans = 0;
+
+                // Process each col
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    switch (i)
+                    {
+                        case 1:
+                            name = fields[i];
+                            break;
+                        case 3:
+                            // Food groups
+                            dietaryConflict = !IsFoodGroupAllowed(fields[i]);
+                            break;
+                        case 9:
+                            protein = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 10:
+                            fat = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 11:
+                            carbs = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 12:
+                            energy = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 16:
+                            sugars = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 27:
+                            saturates = GetProximate(fields[i], ref missingData);
+                            break;
+                        case 45:
+                            trans = GetProximate(fields[i], ref missingData);
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+
+                if (!missingData && !dietaryConflict)
+                {
+                    Portion portion = new(name, energy, protein, fat, carbs, saturates, trans, sugars);
+                    portions.Add(portion);
+                }
+            }
+
+            Console.WriteLine(numMissingDataFields + " fields with missing data.");
+            return portions;
+        }
+    }
+}
---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA;
+
+
+public enum Proximate
+{
+    Energy,
+    Protein,
+    Fat, Saturates, Trans,
+    Carbs, Sugars
+}
+
+
+/// <summary>
+/// Data for each 100g portion of food.
+/// </summary>
+public class Portion
+{
+    // Raw attributes of the food, per 100g.
+    private readonly float _energy;
+    private readonly float _protein;
+    private readonly float _fat, _saturates, _trans;
+    private readonly float _carbs, _sugars;
+
+    public string Name;
+
+    /// <summary>
+    /// Scales every food property, allowing different portion sizes.
+    /// </summary>
+    public float Scale;
+
+
+    /// <summary>
+    /// In Kilocalories (Kcal)
+    /// </summary>
+    public float Energy { get => _energy * Scale; }
+
+    public float Protein { get => _protein * Scale; }
+    public float Fat { get => _fat * Scale; }
+    public float Carbs { get => _carbs * Scale; }
+
+    public float Saturates { get => _saturates * Scale; }
+    public float Trans { get => _trans * Scale; }
+
+    public float Sugars { get => _sugars * Scale; }
+
+
+    public Portion(Portion old)
+    {
+        Name = old.Name;
+        Scale = old.Scale;
+        
+        _energy = old._energy;
+        _protein = old._protein;
+        _fat = old._fat;
+        _carbs = old._carbs;
+        
+        _saturates = old._saturates;
+        _trans = old._trans;
+
+        _sugars = old._sugars;
+    }
+
+
+    public Portion(string name, float energy, float protein, float fat, float carbs, 
+        float saturates, float trans, float sugars, float scale = 1)
+    {
+        Name = name;
+        _energy = energy;
+        _protein = protein;
+        _fat = fat;
+        _carbs = carbs;
+        
+        _saturates = saturates;
+        _trans = trans;
+        
+        _sugars = sugars;
+
+        Scale = scale;
+    }
+
+
+    public float GetProximate(Proximate proximate)
+    {
+        switch (proximate)
+        {
+            case Proximate.Energy:
+                return Energy;
+            case Proximate.Fat:
+                return Fat;
+            case Proximate.Protein:
+                return Protein;
+            case Proximate.Carbs:
+                return Carbs;
+            case Proximate.Saturates:
+                return Saturates;
+            case Proximate.Trans:
+                return Trans;
+            case Proximate.Sugars:
+                return Sugars;
+            default:
+                return 0f;
+        }
+    }
+
+
+    public float Fitness(List<Constraint> constraints, int numMeals, int numPortions)
+    {
+        float fitness = 0f;
+        foreach (Constraint constraint in constraints)
+        {
+            // Multiply the parameter by the number of portions & number of meals to calculate average fitness
+            fitness += constraint.GetFitness(GetProximate(constraint.Parameter) * numMeals * numPortions);
+        }
+        return fitness;
+    }
+
+
+    public void Print()
+    {
+        Console.WriteLine($"{Name}\n Energy: {Energy}kcal, Protein: {Protein}g, Fat: {Fat}g (Saturates: {Saturates}g) (Trans: {Trans}g), Carbs: {Carbs}g (Sugars: {Sugars}g)");
+    }
+}
+
+
+public class Meal
+{
+    public List<Portion> Portions { get; private set; } = new();
+    public int Mutations { get; set; } = 0;
+
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    public Meal(Meal old)
+    {
+        foreach (Portion portion in old.Portions)
+        {
+            Portions.Add(new(portion));
+        }
+
+        LastFitness = old.LastFitness;
+        Mutations = old.Mutations;
+    }
+
+
+    public Meal() { }
+
+
+    /// <summary>
+    /// Cached fitness value, not indicative of current fitness.
+    /// </summary>
+    public float LastFitness { get; private set; } = float.MinValue;
+
+
+    public float TotalFitness(List<Constraint> constraints, int numMeals)
+    {
+        float mealFitness = 0f;
+        foreach (Portion p in Portions)
+        {
+            mealFitness += p.Fitness(constraints, numMeals, Portions.Count);
+        }
+
+        // Inconvenience penalty based on number of portions
+        mealFitness -= MathF.Pow(Portions.Count, 2);
+
+        LastFitness = mealFitness;
+        return mealFitness;
+    }
+
+
+    public void Print()
+    {
+        foreach (Portion portion in Portions)
+        {
+            portion.Print();
+        }
+
+        if (Mutations > 0)
+            Console.WriteLine($"[Mutations: {Mutations}]");
+    }
+
+
+    public static int Compare(Meal a, Meal b)
+    {
+        if (a.LastFitness < b.LastFitness)
+            return -1;
+        else if (a.LastFitness == b.LastFitness)
+            return 0;
+        else
+            return 1;
+    }
+}
